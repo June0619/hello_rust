52356825ba89fd3e92384b32aed43d6b3edb56ed
June0619 <jjw5050@hotmail.com>
2022-04-05 17:10:05 +0900

    GUESS release

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,15 +1,34 @@
+use std::cmp::Ordering;
 use std::io;
+use rand::Rng;
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("### 1부터 100사이의 숫자 맞추기");
 
-    println!("input number > ");
+    loop {
+        println!("숫자를 입력하세요 : ");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("숫자만 입력 가능합니다.");
 
-    io::stdin().read_line(&mut guess)
-        .expect("not inputted number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("your number : {}", guess);
+        println!("입력하신 값 : {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("작습니다."),
+            Ordering::Greater => println!("큽니다."),
+            Ordering::Equal => {
+                println!("정답!");
+                break;
+            },
+        }
+    }
 
 
 }
